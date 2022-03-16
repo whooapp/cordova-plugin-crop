d3ce6541922eaec78bc397c7f96f56718b69b134
Marek Varchola <varchola@wezeo.com>
2022-03-16 14:53:31 +0100

    Android 11 fix -> save cropped to cache dir only

--- src/android/CropPlugin.java
+++ src/android/CropPlugin.java
@@ -90,14 +90,14 @@ public class CropPlugin extends CordovaPlugin {
         File cache = null;
 
         // SD Card Mounted
-        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-            cache = new File(Environment.getExternalStorageDirectory().getAbsolutePath() +
-                    "/Android/data/" + cordova.getActivity().getPackageName() + "/cache/");
-        }
-        // Use internal storage
-        else {
-            cache = cordova.getActivity().getCacheDir();
-        }
+        // if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+        //     cache = new File(Environment.getExternalStorageDirectory().getAbsolutePath() +
+        //             "/Android/data/" + cordova.getActivity().getPackageName() + "/cache/");
+        // }
+        // // Use internal storage
+        // else {
+        cache = cordova.getActivity().getCacheDir();
+        // }
 
         // Create the cache directory if it doesn't exist
         cache.mkdirs();
